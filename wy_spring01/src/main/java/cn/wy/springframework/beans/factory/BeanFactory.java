eba4a2322ea0f6b9bb2e7bff90381d29d17bade8
wangye <202121120206@std.uestc.edu.cn>
2022-11-10 19:51:33 +0800

    01

---
+++ wy_spring01/src/main/java/cn/wy/springframework/beans/factory/BeanFactory.java
@@ -0,0 +1,14 @@
+package cn.wy.springframework.beans.factory;
+
+import cn.wy.springframework.beans.BeansException;
+
+/**
+ * @author wangye
+ * @Email wangye.wy@outlook.com
+ * @CreateTime: 2022-11-10  19:42
+ * @Version: 1.0.0
+ * @Description: TODO
+ */
+public interface BeanFactory {
+    Object getBean(String name) throws BeansException;
+}
---
+++ wy_spring01/src/test/java/cn/wy/springframework/test/bean/UserService.java
@@ -0,0 +1,14 @@
+package cn.wy.springframework.test.bean;
+
+/**
+ * @author wangye
+ * @Email wangye.wy@outlook.com
+ * @CreateTime: 2022-11-10  19:47
+ * @Version: 1.0.0
+ * @Description: TODO
+ */
+public class UserService {
+    public void queryUserInfo(){
+        System.out.println("查询用户信息");
+    }
+}
