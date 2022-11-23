72ca3abc06d798a9699d171b65bc69580a978e29
wangye <202121120206@std.uestc.edu.cn>
2022-11-23 19:17:14 +0800

    init&des

---
+++ wy-spring06/src/main/java/cn/wy/springframework/beans/factory/ListableBeanFactory.java
@@ -0,0 +1,32 @@
+package cn.wy.springframework.beans.factory;
+
+import cn.wy.springframework.beans.BeansException;
+
+import java.util.Map;
+
+/**
+ * @author wangye
+ * @Email wangye.wy@outlook.com
+ * @CreateTime: 2022-11-15  20:39
+ * @Version: 1.0.0
+ * @Description: TODO
+ */
+public interface ListableBeanFactory extends BeanFactory {
+
+    /**
+     * 按照类型返回 Bean 实例
+     * @param type
+     * @param <T>
+     * @return
+     * @throws BeansException
+     */
+    <T> Map<String, T> getBeansOfType(Class<T> type) throws BeansException;
+
+    /**
+     * Return the names of all beans defined in this registry.
+     *
+     * 返回注册表中所有的Bean名称
+     */
+    String[] getBeanDefinitionNames();
+
+}
